38da6631664367b078047995e4b5d81357446387
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-11-21 09:29:30 +0530

    Add files via upload

--- 2.useMemo Example/App.js
+++ 2.useMemo Example/App.js
@@ -1,29 +1,32 @@
 import React, { useEffect, useMemo, useState } from "react";
-import ComA from "./comA"; // Use PascalCase
-import ComB from "./comB"; // Use PascalCase
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [iteam, setIteam] = useState(0);
-  //==>If i Click iteam countMultiple function call every . it is performance issue
-  // function countMultiple() {
-  //   return count * 5;
-  // }
+  const [myNum, setMyNum] = useState(0);
+  const [show, setShow] = useState(true);
+  const getValue = () => {
+    setMyNum(myNum + 1);
+  };
+  const countNumber = (num) => {
+    console.log("Couter num function caling.....");
+    for (let i = 0; i <= 1000000000; i++) {}
+    return num;
+  };
+  //===> we should not write like this , it may be performance issue
+  // const checkData = countNumber(myNum);
 
-  //===>Instead above method we can use useMemo . whenever count value will change that time it will be run . if item value is chnage that time it will be not run .
-  const countMulvalue = useMemo(
-    function countMultiple() {
-      return count * 5;
-    },
-    [count]
-  );
+  //we can use like tahat
+  const checkData = useMemo(() => {
+    return countNumber(myNum);
+  }, [myNum]);
   return (
     <>
-      <h1>Normal Count : {count}</h1>
-      <h1>Normal iteam : {iteam}</h1>
-      <h1>Multple Value:{countMulvalue} </h1>
-      <button onClick={() => setCount(count + 1)}>Count</button>
-      <button onClick={() => setIteam(iteam + 1)}>iteam</button>
+      <button onClick={getValue} style={{ backgroundColor: "red" }}>
+        Counter
+      </button>
+      <p>My new number :{checkData}</p>
+      <button onClick={() => setShow(!show)}>
+        {show ? "You Cliked me " : "Clicked me plz"}
+      </button>
     </>
   );
 }
