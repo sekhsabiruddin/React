e5e8c3e0514c8b7c6467a177977c4c7b9b6e9208
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2024-01-23 12:14:35 +0530

    Add files via upload

---
+++ 21.useCallback/Todos.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { memo } from "react";
+const Todos = ({ todos, addTodo }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo + index}</p>;
+      })}
+      <button onClick={addTodo}>addTodo</button>
+    </>
+  );
+};
+
+export default memo(Todos);
