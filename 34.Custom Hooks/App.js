060024a2eeb30aab1bfa76f4a43369fdc038fdff
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2024-05-21 17:21:50 +0530

    Add files via upload

---
+++ 34.Custom Hooks/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import useFetch from "./useFetch";
+
+function App() {
+  const { data, loading, error } = useFetch("https://api.example.com/data");
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <h1>Fetched Data</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default App;
